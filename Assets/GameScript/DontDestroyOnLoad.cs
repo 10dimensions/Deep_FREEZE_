a7e9636034bb54729f471e3750b11dcea1808644
palashGamer <palash@Palashs-MacBook-Pro.local>
2018-03-11 08:00:03 +0530

    OPening scene interaction completed

---
+++ Assets/GameScript/DontDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyOnLoad : MonoBehaviour {
+
+    [SerializeField] private GameObject[] m_dntDestroyObjects;
+
+    private void Awake()
+    {
+        for (int count = 0; count < m_dntDestroyObjects.Length; count++)
+            DontDestroyOnLoad(m_dntDestroyObjects[count]);
+    }
+    private void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
